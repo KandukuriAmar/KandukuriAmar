04227e94497b457bf5c814d4cd4ed8619567f9d3
KandukuriAmar <2200030959@kluniversity.in>
2023-12-13 20:04:10 +0530

    yes

---
+++ Main.py
@@ -0,0 +1,24 @@
+from Car import Car
+
+c1=Car("Amar","SunRise",2005,"gray")
+c2=Car("Krishna","Sunny",365,"blue")
+
+print("c1 object")
+print(c1.make)
+print(c1.model)
+print(c1.year)
+print(c1.color)
+
+c1.drive()
+c1.stop()
+
+print("c2 object")
+print(c2.make)
+print(c2.model)
+print(c2.year)
+print(c2.color)
+
+c2.drive()
+c2.stop()
+
+
